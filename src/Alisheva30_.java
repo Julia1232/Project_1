d537bb9fd75392b43b0b25695ab08b3e9062e213
Julia1232 <108751081+Julia1232@users.noreply.github.com>
2022-07-13 14:54:29 +0300

    Add files via upload

---
+++ src/Alisheva30_.java
@@ -0,0 +1,19 @@
+public class Alisheva30_ {
+        public static void main(String[] args) {
+            byte x = (byte) 129;
+            System.out.println(x);
+
+            double a = 123.6;
+            int y = (int) a;
+            System.out.println(y);
+
+            long l = Math.round(a);
+            System.out.println(l);
+
+            int b = 123;
+            long l1 = b;
+            int z = (int) l1;
+            System.out.println(z);
+        }
+    }
+
